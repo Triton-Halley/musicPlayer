31b71c081d9f6bb2f279c695a3e0ba09affba963
AliGeshani <Triton.halley@outlook.com>
2020-09-21 04:59:53 +0330

    in my mode i change my model type from Serializable to Parcelable bcu in my PlayMusicFragment i put List in Argument i have flied for music uri and getter and setter

--- app/src/main/java/com/example/MusicPlayer/Model/Music.java
+++ app/src/main/java/com/example/MusicPlayer/Model/Music.java
@@ -1,16 +1,70 @@
 package com.example.MusicPlayer.Model;
 
-import java.io.Serializable;
+import android.net.Uri;
+import android.os.Parcel;
+import android.os.Parcelable;
 
-public class Music implements Serializable {
+import java.io.Serializable;
+import java.util.UUID;
+
+public class Music implements Parcelable{
     private String musicName ;
     private String ArtistName ;
-    // pic
-    // time
+    private UUID musicId;
+    private long duration ;
+    private Uri musicUri ;
 
-    public Music(String musicName, String artistName) {
+    protected Music(Parcel in) {
+        musicName = in.readString();
+        ArtistName = in.readString();
+        duration = in.readLong();
+        musicUri = in.readParcelable(Uri.class.getClassLoader());
+    }
+
+    public static final Creator<Music> CREATOR = new Creator<Music>() {
+        @Override
+        public Music createFromParcel(Parcel in) {
+            return new Music(in);
+        }
+
+        @Override
+        public Music[] newArray(int size) {
+            return new Music[size];
+        }
+    };
+
+    public UUID getMusicId() {
+        return musicId;
+    }
+
+    public void setMusicId(UUID musicId) {
+        this.musicId = musicId;
+    }
+
+    // pic
+
+
+    public Uri getMusicUri() {
+        return musicUri;
+    }
+
+    public void setMusicUri(Uri musicUri) {
+        this.musicUri = musicUri;
+    }
+
+    public Music(String musicName, String artistName, long duration,UUID musicId) {
         this.musicName = musicName;
         ArtistName = artistName;
+        this.duration = duration;
+        this.musicId = musicId;
+    }
+
+    public long getDuration() {
+        return duration;
+    }
+
+    public void setDuration(long duration) {
+        this.duration = duration;
     }
 
     public String getMusicName() {
@@ -27,5 +81,18 @@ public class Music implements Serializable {
 
     public void setArtistName(String artistName) {
         ArtistName = artistName;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(musicName);
+        dest.writeString(ArtistName);
+        dest.writeLong(duration);
+        dest.writeParcelable(musicUri, flags);
     }
 }
