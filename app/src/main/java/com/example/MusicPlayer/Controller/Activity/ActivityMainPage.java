8071c597fcaf8a071ec73b8062134714706afa5b
AliGeshani <Triton.halley@outlook.com>
2020-09-26 17:30:55 +0330

    i create a permission runtime and when user don't give the permission to application this app not working i use EasyPermission Library i call onRequestPermissionResult and implements EasyPermission.PermissionCallBacks interface and implements two method onPermissionsGranted and onPermissionsDenied and when user give access to application i call may setUI method in onPermissionsGranted

--- app/src/main/java/com/example/MusicPlayer/Controller/Activity/ActivityMainPage.java
+++ app/src/main/java/com/example/MusicPlayer/Controller/Activity/ActivityMainPage.java
@@ -1,22 +1,37 @@
 package com.example.MusicPlayer.Controller.Activity;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 import androidx.viewpager2.widget.ViewPager2;
 
+import android.Manifest;
+import android.os.Build;
 import android.os.Bundle;
 
-import com.example.MusicPlayer.Controller.Fragment.MainFragment;
+import com.example.MusicPlayer.Controller.Fragment.ListOfArtistsFragment;
+import com.example.MusicPlayer.Controller.Fragment.ListOfMusicFragment;
 import com.example.MusicPlayer.R;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
 
-public class ActivityMainPage extends AppCompatActivity {
-    private ViewPager2 mTaskViewPager;
+import java.util.List;
 
+import pub.devrel.easypermissions.EasyPermissions;
+
+public class ActivityMainPage extends AppCompatActivity implements EasyPermissions.PermissionCallbacks {
+    private ViewPager2 mTaskViewPager;
+/*    public static final int REQUEST_CODE_EASY_PERMISSION = 123 ;
+    public static int REQUEST_CODE_ = 0 ;*/
+/*    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+    }*/
+
+    @RequiresApi(api = Build.VERSION_CODES.R)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,9 +44,22 @@ public class ActivityMainPage extends AppCompatActivity {
         mTaskViewPager = findViewById(R.id.viewPager2);
     }
 
+    // in annotation automatically call setUI and we don't need to call it again
+    //Idk why but this Annotation not Working i call this method inside onPermissionGranted method
+//    @AfterPermissionGranted(123)
     private void setUI() {
-        FragmentStateAdapter adapter = new PagerAdapter(this);
-        mTaskViewPager.setAdapter(adapter);
+        String[] permissions ={Manifest.permission.READ_EXTERNAL_STORAGE};
+        if (EasyPermissions.hasPermissions(this,permissions)){
+            FragmentStateAdapter adapter = new PagerAdapter(this);
+            mTaskViewPager.setAdapter(adapter);
+            defineTabs();
+        }else {
+            EasyPermissions.requestPermissions(this,
+                    "PermissionDenied need access to Storage",132,permissions);
+        }
+    }
+
+    private void defineTabs() {
         TabLayout tabLayout = findViewById(R.id.tab_layout);
         TabLayoutMediator tabLayoutMediator = new TabLayoutMediator(
                 tabLayout, mTaskViewPager, new TabLayoutMediator.TabConfigurationStrategy() {
@@ -56,8 +84,26 @@ public class ActivityMainPage extends AppCompatActivity {
         }
         );
         tabLayoutMediator.attach();
-
     }
+
+    @Override
+    public void onPermissionsGranted(int requestCode, @NonNull List<String> perms) {
+        setUI();
+    }
+
+    @Override
+    public void onPermissionsDenied(int requestCode, @NonNull List<String> perms) {
+/*        if (EasyPermissions.somePermissionPermanentlyDenied(this,perms)){
+            new AppSettingsDialog.Builder(this).build().show();
+        }*/
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        EasyPermissions.onRequestPermissionsResult(requestCode,permissions,grantResults,this);
+    }
+
     private class PagerAdapter extends FragmentStateAdapter {
 
         public PagerAdapter(@NonNull FragmentActivity fragmentActivity) {
@@ -69,13 +115,13 @@ public class ActivityMainPage extends AppCompatActivity {
         public Fragment createFragment(int position) {
             switch (position) {
                 case 0: {
-                    return MainFragment.newInstance("");
+                    return ListOfMusicFragment.newInstance();
                 }
                 case 1: {
-                    return MainFragment.newInstance(" ");
+                    return ListOfArtistsFragment.newInstance("Album");
                 }
                 default: {
-                    return MainFragment.newInstance("  ");
+                    return ListOfArtistsFragment.newInstance("Artist");
                 }
             }
         }
@@ -85,6 +131,7 @@ public class ActivityMainPage extends AppCompatActivity {
             return 3;
         }
     }
+
 }
 
 
